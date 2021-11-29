f6b4e02cd9636c863ee1971146a0a5d8a05a78eb
dimen <dimenerno@gmail.com>
2021-11-29 16:48:20 +0900

    Add doughnut chart

--- src/components/Reports.js
+++ src/components/Reports.js
@@ -1,11 +1,19 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
 import formatTime from '../helper/formatTime'
+import { Chart, ArcElement } from 'chart.js'
+import { Doughnut } from "react-chartjs-2";
 
 const Reports = () => {
-
+    Chart.register(ArcElement);
     const [list_of_category, set_list_of_category] = useState([])
     const [list_of_duration, set_list_of_duration] = useState([])
+    const [chartData, setchartData] = useState({
+        labels: [],
+        datasets: []
+    })
+
+    
 
     useEffect(() => {
         fetch('http://localhost:9000')
@@ -14,13 +22,36 @@ const Reports = () => {
                 const report = data.report
                 var category_buffer = [(<th>Category</th>)]
                 var duration_buffer = [(<th>Duration</th>)]
+                var labels = []
+                var dataset = []
 
                 for(var key in report) {
+                    labels.push(key)
+                    dataset.push(report[key])
                     category_buffer.push((<td key={key}>{key}</td>))
+
                     const formatted_time = formatTime(report[key])
                     duration_buffer.push((<td key={key}>{formatted_time.hours}:{formatted_time.minutes}:{formatted_time.seconds}</td>))
                 }
 
+                setchartData({
+                    labels: labels,
+                    datasets: [
+                        {
+                          label: 'Time spent at each category',
+                          data: dataset,
+                          backgroundColor: [
+                            '#AC92EB',
+                            '#4FC1E8',
+                            '#A0D568',
+                            '#FFCE54',
+                            '#ED5564'
+                          ],
+                          borderWidth: 0,
+                        }
+                    ]
+                })
+                
                 set_list_of_category(category_buffer)
                 set_list_of_duration(duration_buffer)
             })
@@ -29,16 +60,22 @@ const Reports = () => {
     return (
         <div className="wrapper">
             <div className="reports">
-                <table>
-                    <tbody>
-                        <tr>
-                            {list_of_category}
-                        </tr>
-                        <tr>
-                            {list_of_duration}
-                        </tr>
-                    </tbody>
-                </table>
+                <div className="chart">
+                    <Doughnut data={chartData} options={{
+                    }}/>
+                </div>
+                <div>
+                    <table>
+                        <tbody>
+                            <tr>
+                                {list_of_category}
+                            </tr>
+                            <tr>
+                                {list_of_duration}
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
     )
