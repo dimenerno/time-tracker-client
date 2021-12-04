5f3774f6204bf7c39d694e8884f642522eaa315d
dimen <dimenerno@gmail.com>
2021-12-04 23:41:05 +0900

    Minor changes (add tab color)

--- src/components/ReportsYear.js
+++ src/components/ReportsYear.js
@@ -17,8 +17,6 @@ const ReportsYear = ({ viewBy }) => {
         datasets: []
     })
     const [dateDelta, setdateDelta] = useState(0)
-    const months = [ "", "January", "February", "March", "April", "May", "June", 
-           "July", "August", "September", "October", "November", "December" ];
 
     useEffect(() => {
         fetch(`http://localhost:9000?year=${displayYear}`)
--- src/index.js
+++ src/index.js
@@ -2,9 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import MetaTags from 'react-meta-tags';
 
 ReactDOM.render(
   <React.StrictMode>
+    <MetaTags>
+      <title>Page 1</title>
+      <meta name="theme-color" content="#222222" />
+    </MetaTags>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
